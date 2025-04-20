5fc0044ba1ffa46d73d541857bebfb85c112b047
jayce <jayce.baxter@proton.me>
2025-04-20 12:13:17 -0400

    changed discord to vesktop in rofi menu

--- i3/custom_rofi.sh
+++ i3/custom_rofi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apps="firefox\nnautilus\nsteam\ndiscord"
+apps="firefox\nnautilus\nsteam\nvesktop"
 
 selected=$(echo -e "$apps" | rofi -dmenu -p "💙" -theme /home/jayce/.config/rofi/custom-theme.rasi)
 
@@ -8,7 +8,7 @@ case $selected in
     firefox) firefox & ;;
     nautilus) nautilus & ;;
     steam) steam & ;;
-    discord) discord & ;;
+    vesktop) vesktop & ;;
     *) exit 1 ;;
 esac
 
